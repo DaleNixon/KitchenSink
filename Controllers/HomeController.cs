f2d6873ff6af5b1ca8b9a8838a36d91b8692afb7
John Vernon <jwvernon@gmail.com>
2019-05-18 16:33:10 -0500

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using KitchenSink.Models;
+
+namespace KitchenSink.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Promise()
+        {
+            return View();
+        }
+
+        public ActionResult LongRunningTask()
+        {
+            var time = (new Random().Next(5, 20)) * 100;
+
+            // Sleep for time seconds to simulate a long-running task
+            System.Threading.Thread.Sleep(time);
+
+            // 10% chance of this method throwing an exception
+            if (new Random().Next(0, 10) == 5) {
+                throw new TaskCanceledException("The task was cancelled by the system.");
+            }
+
+            // 50% chance of returning true for success
+            return Json(new { Success = (time % 2000 == 0) });
+        }
+    }
+}
